b66f704769c2b0e10dfacd4aecd02381d6e1ff65
testtanweer <tancero@proton.me>
2026-02-18 02:37:11 +0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/movies-app',
-  assetPrefix: '/movies-app/', // Add this line with the trailing slash
-  images: { unoptimized: true },
-}
-module.exports = nextConfig
